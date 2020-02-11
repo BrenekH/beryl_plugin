cdae325a3f5534456a5d9d4293d80e1cae3f5d1a
Brenek Harrison <brenekharrison@gmail.com>
2020-02-10 20:17:29 -0700

    Moving code to seperate repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='beryl_plugin',
+      version='0.0.0-alpha.0',
+      description='A helper library for creating plugins for the Beryl Timing System.',
+      url='http://github.com/zPaw/beryl-timing-system',
+      author='Brenek Harrison',
+      author_email='brenekharrison@gmail.com',
+      license='GPLv3',
+      packages=['beryl_plugin'],
+      zip_safe=False)
