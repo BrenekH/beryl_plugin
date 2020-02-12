e170873c6ff255f739c6d55ae7ef41cfb959de3b
Brenek Harrison <brenekharrison@gmail.com>
2020-02-11 22:02:40 -0700

    Fix pytest bug

--- tests/test_version_is_not_already_on_pypi.py
+++ tests/test_version_is_not_already_on_pypi.py
@@ -4,7 +4,7 @@ from pkg_resources import safe_version
 from beryl_plugin import __version__
 
 def test_version_is_not_already_on_pypi():
-	_, pip_output = subprocess.getstatusoutput([sys.executable, '-m', 'pip', 'install', 'beryl-plugin=='])
+	_, pip_output = subprocess.getstatusoutput(f"{sys.executable} -m pip install beryl-plugin==")
 	print(pip_output)
 	available_versions = pip_output.split("(")[1].split(")")[0].replace("from versions: ", "").split(", ")
 	print(available_versions)
