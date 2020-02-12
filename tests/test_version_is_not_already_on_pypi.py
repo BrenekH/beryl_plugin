c9f36593801e1c1b4925a1111e058b92f3e2a66b
Brenek Harrison <brenekharrison@gmail.com>
2020-02-11 21:41:17 -0700

    Update .travis.yml

--- tests/test_version_is_not_already_on_pypi.py
+++ tests/test_version_is_not_already_on_pypi.py
@@ -5,6 +5,7 @@ from beryl_plugin import __version__
 
 def test_version_is_not_already_on_pypi():
 	_, pip_output = subprocess.getstatusoutput([sys.executable, '-m', 'pip', 'install', 'beryl-plugin=='])
+	print(pip_output)
 	available_versions = pip_output.split("(")[1].split(")")[0].replace("from versions: ", "").split(", ")
 	print(available_versions)
 	assert not safe_version(__version__) in available_versions
