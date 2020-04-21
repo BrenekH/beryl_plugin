17410e5bd90b1703eadd6b90106597e1b7fda5fd
Brenek Harrison <brenekharrison@gmail.com>
2020-04-21 12:51:56 -0600

    Change to new plugin format

--- beryl_plugin/__init__.py
+++ beryl_plugin/__init__.py
@@ -3,21 +3,12 @@ import abc
 __version__ = "0.0.1-alpha.7"
 
 class PluginBase(abc.ABC):
-	def __init__(self, spawning_class, name, uuid, family_id):
+	def __init__(self, spawning_class, display_name, ID):
 		self._super_class = spawning_class
-		self._child_name = name
-		self._child_uuid = uuid
-		self._child_family = family_id
-
-	# No need to override methods
-	def info(self):
-		return {"name": self._child_name, "uuid": self._child_uuid, "family_id": self._child_family}
+		self._child_display_name = display_name
+		self.ID = ID
 
 	# Required to override
-	@abc.abstractmethod
-	def register_listeners(self):
-		pass
-
 	@abc.abstractmethod
 	def stop(self):
 		"""Called when the plugin is no longer needed. Should properly close any open streams.
@@ -25,31 +16,18 @@ class PluginBase(abc.ABC):
 		pass
 
 	# API Methods
-	# Listeners
-	def register_key_listener(self, key, listener):
-		return self._super_class.register_key_listener(key, listener)
-
-	def register_period_change_listener(self, listener):
-		return self._super_class.register_period_change_listener(listener)
-
-	def register_on_loop_listener(self, listener):
-		return self._super_class.register_on_loop_listener(listener)
-
-	def get_cross_plugin_data(self, key):
-		return self._super_class.get_cross_plugin_data(self, key)
-
-	def save_cross_plugin_data(self, key, value):
-		return self._super_class.save_cross_plugin_data(self, key, value)
-
-	
 	# Configurations
 	def get_current_config(self):
 		return self._super_class.get_current_config(self)
-
-	def get_current_family_config(self):
-		return self._super_class.get_current_family_config(self)
-
 	
 	# Display related methods
 	def get_plugin_display(self):
 		return self._super_class.get_plugin_display(self)
+
+class Event:
+	key = "key"
+	loop = "loop"
+	period_change = "period change"
+	config_change = "config change"
+
+	_all_events = [key, loop, period_change, config_change]
